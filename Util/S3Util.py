4e74035ffa95b396087799a1d304a9678a0aa078
Videlefsky <j.videlefsky@northeastern.edu>
2023-03-24 08:22:30 -0400

    S3 testing

--- Util/S3Util.py
+++ Util/S3Util.py
@@ -11,7 +11,10 @@ aws_secret_access_key = config('aws_secret_access_key')
 
 class S3Util:
     def __init__(self, resource, bucket_name):
-        self.s3 = boto3.client(resource, config=Config(signature_version=UNSIGNED))
+        # self.s3 = boto3.client(resource, config=Config(signature_version=UNSIGNED))
+        self.s3 = boto3.client(resource, 
+                               aws_access_key_id=aws_access_key_id,
+                               aws_secret_access_key=aws_secret_access_key)
         self.bucket_name = bucket_name
 
     def upload_file(self, file_name, bucket, object_name=None):
@@ -19,8 +22,10 @@ class S3Util:
         if object_name is None:
             object_name = os.path.basename(file_name)
 
+        print('test1')
         # Upload the file
         s3_client = boto3.client('s3')
+        print('test2')
         # try:
         response = s3_client.upload_file(file_name, bucket, object_name)
         # except ClientError as e:
@@ -42,3 +47,15 @@ class S3Util:
     #     )
     #     return url
 
+
+
+#########################################
+# # SCRAP
+# s3Util = S3Util('s3', 'damg7245-assignment4')
+
+# dir(s3Util)
+# s3Util.bucket_name
+# s3Util.s3.upload_file('JV_AUDIO_EXAMPLE.WAV', 'damg7245-assignment4', 'Uploaded_Audio_Files/JV_AUDIO_EXAMPLE.WAV')
+
+# # Why Doesn't this work?
+# s3Util.upload_file('JV_AUDIO_EXAMPLE.WAV', 'In_Audio_Files')
