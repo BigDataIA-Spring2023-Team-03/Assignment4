7c9f043488511334cf8241200e1b46626eda3a7a
JaredVidelefsky <jared.videlefsky@gmail.com>
2023-03-29 22:29:10 -0400

    Analytics Dashboard and other work

--- streamlit/pages/2_Analytics_Dashboard.py
+++ streamlit/pages/2_Analytics_Dashboard.py
@@ -6,6 +6,16 @@ import requests
 import time
 from main import check_dag_status
 import json
+import seaborn as sns
+import pandas as pd
+
+# DEV or PROD
+environment = 'DEV'
+if environment == 'DEV':
+    webserver = 'localhost:8080'
+elif environment == 'PROD':
+    webserver = 'airflow-airflow-webserver-1:8080'
+
 
 # Load the environment variables from the .env file
 load_dotenv()
@@ -45,8 +55,58 @@ password = st.text_input("Enter Password:")
 
 if password != '':
     if password == 'damgadmin': 
-        # Usage Graph
-        st.write('Usage Graph:')
+    
+        # ######################################################################################################
+        # TODO: 100 MOST RECENT EVENTS, WILL NEED TO LOOP THROUGH
+        url = f"http://{webserver}/api/v1/eventLogs?limit=1000&order_by=-when"
+
+        response = requests.get(url=url, auth=('airflow2', 'airflow2'))
+
+        data = response.json()
+        event_df = pd.json_normalize(data, record_path =['event_logs'])
+        st.write(f'Total Events: {event_df.shape[0]}')
+        st.write(event_df)
+
+        # User Requests Over Time
+        st.header('Requests Over Time')
+        summary = event_df.loc[(event_df['dag_id'].isin(['audio_transcription', 'audio_transcription_batch'])) & (event_df['event'].isin(['failed', 'success'])),
+                    ['dag_id','event','execution_date']]
+        st.write(summary.drop_duplicates())
+
+        # fig = plt.figure(figsize=(10, 4))
+        # sns.lineplot(x='date', y='total_requests', hue='email', 
+        #             data=df,
+        #             markers=True).set(ylim=(0))
+        # st.pyplot(fig)
+
+
+        # ######################################################################################################
+        # # Total Requests Over Time
+        # # TODO: turn into bar graph where you can see the breakdown of success/failure request status
+        # st.header('Usage Over Time')
+
+        # query = f"""
+        # select date(time_of_request) date, 
+        #         count(distinct email) total_users,
+        #         count(*) total_requests
+        # from user_api 
+        # group by date
+        # """
+        # df = pd.read_sql_query(query, conn)
+        # # TESTING
+        # # st.write(df.head())
+        # # st.write(df.dtypes)
+
+        # fig = plt.figure(figsize=(10, 4))
+        # g = sns.lineplot(x=df.date, y=df.total_requests, color="g")
+        # sns.lineplot(x=df.date, y=df.total_users, color="b", ax=g.axes.twinx())
+        # g.legend(handles=[Line2D([], [], marker='_', color="g", label='total_requests'), 
+        #                 Line2D([], [], marker='_', color="b", label='total_users')])
+
+        # st.pyplot(fig)
+
+
+        # ######################################################################################################
         
     else:
         st.error("You don't have permission to access this functionality")
